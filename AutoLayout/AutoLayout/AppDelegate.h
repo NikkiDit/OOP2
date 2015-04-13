dde293d756c4607bdde03a855e3e8a5b4e96d35d
Adenike Olatunji <d10125267@ast-1027-m18.ict.ad.dit.ie>
2015-04-13 20:00:57 +0100

    Initial Commit

---
+++ AutoLayout/AutoLayout/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AutoLayout
+//
+//  Created by Adenike Olatunji on 13/04/2015.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AutoLayout/AutoLayout/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AutoLayout
+//
+//  Created by Adenike Olatunji on 13/04/2015.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
